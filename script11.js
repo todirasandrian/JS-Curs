8bb6cc98527409b40620af218e5858c83dedcd79
Todiras Andrian <todirasandrian1@gmail.com>
2021-08-09 23:49:35 +0200

    first comit

---
+++ script11.js
@@ -0,0 +1,29 @@
+//const leftBtn = document.querySelector('#btn-left');
+//let leftMoove = 0;
+//let timer;
+
+////leftBtn.addEventListener('click' , function() {
+////	const scrolArea = document.querySelector('#scrool')
+////	leftMoove = leftMoove - 128;
+////	if (leftMoove < -256) {
+////		leftMoove = 0;
+////	}
+////	scrolAreal.style.left = leftMoove + 'px';
+////})
+
+
+////auto!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//autoSlider ();
+//function autoSlider () {
+//	timer = setTimeout (function() {
+//		const scrolArea = document.querySelector('#scrool');
+//		leftMoove = leftMoove - 128;
+//		if (leftMoove < -256) {
+//			leftMoove = 0;
+//			clearTimeout(timer);
+//		}
+//		scrolArea.style.left = leftMoove + 'px';
+//		autoSlider();
+//	}, 1000)
+//}
+////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
